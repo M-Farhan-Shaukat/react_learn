b6ae5d0bbd32fd660247612ae347eaf70bfc2f3b
M Farhan Shaukat <100473373+M-Farhan-Shaukat@users.noreply.github.com>
2024-12-18 17:50:38 +0500

    sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,14 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+
+const sitemap = new SitemapStream({ hostname: 'https://charcount-lime.vercel.app/' });
+
+sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+sitemap.write({ url: '/character-counter', changefreq: 'daily', priority: 0.8 });
+
+sitemap.end();
+
+streamToPromise(sitemap).then((data) => {
+  createWriteStream('./public/sitemap.xml').write(data);
+  console.log('Sitemap created successfully.');
+});
